56775cc8bb13bb5b5fea36e8d2dada4598531571
byeongjun2 <byeongjun1326@gmail.com>
2025-04-02 18:48:54 +0900

    MapManager #1

--- CodingRun/Assets/Script/MapLoad/MapManager.cs
+++ CodingRun/Assets/Script/MapLoad/MapManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MapManager : MonoBehaviour
 {
     public static MapManager instance;
-
-    public List<GameObject> mapPrefs;
+    [Range(1, 10)]
+    public int loadNum = 3;
+    [SerializeField]
+    private Queue<GameObject> roadPool = new Queue<GameObject>();
+    public List<GameObject> mapPrefs = new List<GameObject>();
     private List<Quiz> quizList;
     [SerializeField]
     private GameObject currentMap;
@@ -24,12 +28,45 @@ public class MapManager : MonoBehaviour
         return new Quiz();
     }
 
-    public void StartMap() {
-        //처음 시작할때 맵을 불러오는 함수.
+    public void InitObjectPool() {
+        if (mapPrefs == null) {
+            Debug.LogError("mapPref is null!");
+            return;
+        }
+
+        for (int i = 0; i < loadNum; i++) {
+            GameObject road = Instantiate(mapPrefs[0]);
+            road.SetActive(true);
+            roadPool.Enqueue(road);
+        }
+
+        Debug.Log("RoadPool is initialized!");
+    }
+    /// <summary>
+    /// returns GameObejct's Vector3 size data.
+    /// </summary>
+    /// <param name="obj">GameObject what want to get size</param>
+    /// <returns>Size Vector3 Data</returns>
+    private Vector3 GetObjectSize(GameObject obj) {
+        Collider collider = obj.GetComponent<Collider>();
+        if (collider == null) {
+            Debug.Log("Failed to find Collider Component. trying to find in child Object...");
+            collider = obj.GetComponentInChildren<Collider>();
+            if (collider == null) {
+                Debug.LogError("collider is null! check the component!");
+                return new Vector3(0f, 0f, 0f);
+            }
+        }
+        return collider.bounds.size;
     }
 
-    public void RefreshMap() {
-        //안보이는 곳을 삭제하고, 새로 보일 맵을 생성하는 함수. (무한 맵)
+    private void InitMap() {
+        
+    }
+
+    void Start()
+    {   
+        InitObjectPool(); //Road 오브젝트 풀 로드
     }
 
     void Awake()
