d440369123b4bb9515fe99e37cd6f1dacc3902af
ogeonu21 <167061004+ogeonu21@users.noreply.github.com>
2025-04-02 19:39:55 +0900

    오브젝트 생성 및 움직임 로직 형성
    오브젝트를 일정 주기로 생성하고 자동으로 움직여 맵 밖으로 벗어날 경우 파괴하는 로직을 생성하였다.

--- CodingRun/Assets/Script/MapLoad/StageManager.cs
+++ CodingRun/Assets/Script/MapLoad/StageManager.cs
@@ -6,16 +6,24 @@ public class StageManager : MonoBehaviour
 {
     private Vector3[] spawnPoint;
     private float objectSpeed;
+    private float coinTimer;
+    private float waitingCoinTime;
 
     [SerializeField]
     public GameObject testPrefab1;
 
     void Awake()
     {
-        //임시 spawnPoint 설정
-        objectSpeed = 4.0f;
+        
+        objectSpeed = 5.0f;
+        coinTimer = 0.0f;
+        waitingCoinTime = 0.8f;
+
+
         spawnPoint = new Vector3[3];
 
+
+        //임시 spawnPoint 설정
         spawnPoint[0] = new Vector3(-4.9f, 1f, 14f);
         spawnPoint[1] = new Vector3(0f, 1f, 14f);
         spawnPoint[2] = new Vector3(4.9f, 1f, 14f);
@@ -28,10 +36,13 @@ public class StageManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            
+        //0.8초 주기로 코인 생성
+        coinTimer += Time.deltaTime;
+        if (coinTimer > waitingCoinTime) {
+            SpawnCoin();
+            coinTimer = 0.0f;
         }
+        
     }
 
     private void SpawnCoin()
