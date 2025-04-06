641f510e0ff853a4b73b8d95ea0063cabe5ec07b
이주한 <161941871+LH99Tw@users.noreply.github.com>
2025-04-07 03:43:12 +0900

    Q_SO 수정, Quiz 에셋 추가
    27개 에셋 추가와 에셋 변수 변경

--- CodingRun/Assets/Script/Quiz/QuestionSO.cs
+++ CodingRun/Assets/Script/Quiz/QuestionSO.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum QuestionDifficulty
+{
+    Easy,
+    Medium,
+    Hard
+}
+
 [CreateAssetMenu(menuName = "QuizQuestion", fileName = "new Question")]
 public class QuestionSO : ScriptableObject
 {
-
+    [SerializeField] int questionNumber;
+    [SerializeField] QuestionDifficulty difficulty = QuestionDifficulty.Medium;
+    
     [TextArea(2,6)]
     [SerializeField] string question = "Enter question";
     [SerializeField] string[] answers = new string[3];
@@ -21,5 +30,14 @@ public class QuestionSO : ScriptableObject
 
     public int GetCorrectIndex(){
         return correctAnswerIndex;
+    }
+
+    public int GetQuestionNumber(){
+        return questionNumber;
+    }
+
+    public QuestionDifficulty GetDifficulty()
+    {
+        return difficulty;
     }
 }
