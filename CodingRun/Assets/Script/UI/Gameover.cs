bf9a44802fd56197493f36f387b81ea44f6b8574
Heonyong Jeung <combo124@naver.com>
2025-05-18 02:46:41 +0900

    게임오버ui 수정
    time에서 coin의 개수가 출력하게 하는 코드를 넣었습니다.
    혹시 몰라 time으로 출력하게 하도록 놔두었습니다.

--- CodingRun/Assets/Script/UI/Gameover.cs
+++ CodingRun/Assets/Script/UI/Gameover.cs
@@ -9,15 +9,23 @@ public class Gameover : MonoBehaviour
     [SerializeField] private TextMeshProUGUI endScoreText;
     [SerializeField] private TextMeshProUGUI endCoinText;
 
-
+                                                             
     public void ShowGameOverUI(float score, float time)
     {
+        //coin 출력할때 주석 풀어주시면 됩니다
+        //int coin = GameManager.Instance.InGameCoin; //  coin 변수 이거 주석처리 되어 있으면 코인 출력 안됩니다!!
+
+
 
         // UI 켜기
         endScreenCanvas.SetActive(true);
 
         // 텍스트 업데이트
         endScoreText.text = $"Score: {score:F1}";
-        endCoinText.text = $"Play Time: {time}";
+        endCoinText.text = $"Play Time: {time}";//(코인 출력하실때 이 코드 주석 처리하면 됩니다)
+     //endCoinText.text = $"Coin: {coin}"; //코인 출력하게 싶다면 이 코드 주석 해제하고 위 코드도 주석 해제하면 됩니다 (5월18일 ui 정헌용 작성)
+
+      
+
     }
 }
